732dac0f04071e6af070b617232f916b2f24421f
DuongKwang <dnquang203@gmail.com>
2023-08-09 16:52:35 +0700

    Change

--- app/Http/Controllers/Account.php
+++ app/Http/Controllers/Account.php
@@ -111,10 +111,10 @@ class Account extends Controller
                 $newUser->role_id = $request->role;
                 $newUser->save();
 
-                return redirect()->route('welcome.login')->with('message', 'Create success!');
+                return redirect()->route('welcome.login')->with('message', 'Create Success!!');
             } else {
 
-                return redirect()->route('welcome.register')->with('message', 'Create failed!');
+                return redirect()->route('welcome.register')->with('message', 'Create Failed!!');
 
             }
 
